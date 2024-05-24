3002b1ec1bc4b8cb48f4e623b7a83a02dadfacd5
g3-tin <tin@platfarm.net>
2024-05-24 13:14:05 +0700

    chore: Update MANIFEST_URL in Providers component

--- apps/app/src/app/_providers/index.tsx
+++ apps/app/src/app/_providers/index.tsx
@@ -5,7 +5,8 @@ import { TRPCReactProvider } from "../../trpc/react";
 import { BackendAuthProvider } from "./BackendAuthProvider";
 
 // Manifest URL must be https with a public domain
-const MANIFEST_URL = "https://g3-miniapp.vercel.app/tonconnect-manifest.json";
+const MANIFEST_URL =
+  "https://staging.miniapp.gall3ry.io/tonconnect-manifest.json";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -13,7 +14,7 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
       <TonConnectUIProvider
         manifestUrl={MANIFEST_URL}
         // actionsConfiguration={{
-        //   twaReturnUrl: "https://t.me/g3stgbot/demo",
+        //   twaReturnUrl: "https://t.me/g3stgbot",
         // }}
       >
         <BackendAuthProvider>{children}</BackendAuthProvider>

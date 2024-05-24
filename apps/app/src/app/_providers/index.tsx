3d3aaf330a62023fcba2b899562518ab0e242b4c
g3-tin <tin@platfarm.net>
2024-05-24 14:06:36 +0700

    chore: Update Gall3ry Staging manifest URL and icon

--- apps/app/src/app/_providers/index.tsx
+++ apps/app/src/app/_providers/index.tsx
@@ -6,7 +6,7 @@ import { BackendAuthProvider } from "./BackendAuthProvider";
 
 // Manifest URL must be https with a public domain
 const MANIFEST_URL =
-  "https://gist.githubusercontent.com/g3-tin/635922e04226c2ce116a2f0586b46858/raw/6351ce742bbe0f82d99bae93e5095eb8efc1c441/tonconnect-manifest.json";
+  "https://staging.miniapp.gall3ry.io/tonconnect-manifest.json";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (

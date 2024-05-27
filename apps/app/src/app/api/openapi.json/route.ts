9005bebbe5da38806d03c577864e9dcc87db5582
g3-tin <tin@platfarm.net>
2024-05-27 11:20:40 +0700

    chore: Remove unused dependencies and files related to trpc-openapi

--- apps/app/src/app/api/openapi.json/route.ts
+++
@@ -1,8 +0,0 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
-import { openApiDocument } from "../../../server/api/openapi";
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).send(openApiDocument);
-};
-
-export { handler as GET, handler as POST };

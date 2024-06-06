56680c01b5184e2078e652f81763ea0a5a176a36
g3-tin <tin@platfarm.net>
2024-06-06 13:49:56 +0700

    fix: 🐛 build fixes

---
+++ apps/app/src/app/(home)/MyCurrentPosition.tsx
@@ -0,0 +1,32 @@
+import { Spinner } from "@radix-ui/themes";
+import { api } from "../../trpc/react";
+import { IMAGES } from "../_constants/image";
+import { LeaderboardItem } from "./LeaderboardItem";
+
+export const MyCurrentPosition = () => {
+  const { data, isPending, isSuccess } =
+    api.occ.getMyCurrentLeaderboardPosition.useQuery();
+
+  return (
+    <Spinner loading={isPending}>
+      {isSuccess && (
+        <div className="rounded-lg bg-[#FCFFE5] p-3">
+          <div className="text-base font-bold leading-normal text-slate-900">
+            Your current position:
+          </div>
+
+          <div className="mt-8">
+            <LeaderboardItem
+              avatarUrl={data.avatarUrl}
+              occImageUrl={(IMAGES.MOCK_OCC as any)[data.rank % 5]}
+              rank={data.rank}
+              shareCount={data.shareCount}
+              username={data.username}
+              address={data.address}
+            />
+          </div>
+        </div>
+      )}
+    </Spinner>
+  );
+};
